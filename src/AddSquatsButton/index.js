8ba876e1ae43dd09bd26ade1ce7e6523e54770c1
olgaKhristo <olga84h@gmail.com>
2023-05-03 16:59:12 +0100

    start

---
+++ src/AddSquatsButton/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function AddSquatsButton({numberOfSquats}){
+    return(
+        <button>
+            Add {numberOfSquats}
+        </button>
+    );
+}
+export default AddSquatsButton;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import AddSquatsButton  from './AddSquatsButton';
+import { useState } from 'react';
+
 
 function App() {
+  const [squatCount, setSquatCount] = useState(0);
+function addSquart(squatNum){
+  const newCount = squatCount + squatNum;
+  setSquatCount(newCount)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Heading here</h1>
+      <img id='logo' src="https://i.ibb.co/ZfnfqHP/school-of-suats.png" style={{height:200, width:200}} alt='some img here' />
+    <h2 className='squats-number-display'>You done {squatCount} squats so far. Keep it going!</h2>
+      <section className='bth-here'>
+     <AddSquatsButton numberOfSquats={1}/>
+     <AddSquatsButton numberOfSquats={5}/>
+     <AddSquatsButton numberOfSquats={10}/>
+      <button className='reset-butn'>Reset button</button>
+
+
+      </section>
+    
     </div>
   );
 }
