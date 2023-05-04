951cf5da3fba0d925075677408cae6a1784f2186
olgaKhristo <olga84h@gmail.com>
2023-05-04 20:06:47 +0100

    added acordion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import AddSquatsButton  from './AddSquatsButton';
 import { useState } from 'react';
+import Button from '@mui/material/Button';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 
 function App() {
@@ -16,6 +22,7 @@ function addSquart(squatNum){
       <img id='logo' src="https://i.ibb.co/ZfnfqHP/school-of-suats.png" style={{height:200, width:200}} alt='some img here' />
     <h2 className='squats-number-display'>You done {squatCount} squats so far. Keep it going!</h2>
       <section className='bth-here'>
+      <Button variant="contained">Hello World</Button>
      <AddSquatsButton numberOfSquats={1}/>
      <AddSquatsButton numberOfSquats={5}/>
      <AddSquatsButton numberOfSquats={10}/>
@@ -23,6 +30,47 @@ function addSquart(squatNum){
 
 
       </section>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Olga</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion disabled>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Disabled Accordion</Typography>
+        </AccordionSummary>
+      </Accordion>
     
     </div>
   );
